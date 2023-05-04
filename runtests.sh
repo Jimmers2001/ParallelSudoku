fa7912266c89597ddc66fc11d5155976e3d4f8fd
Jimmers2001 <38543433+Jimmers2001@users.noreply.github.com>
2023-05-04 18:20:20 -0400

    RUNS ON THE SUPERCOMPUTER

--- runtests.sh
+++ runtests.sh
@@ -6,7 +6,6 @@ make FILE_NAME="sample_tests/9x9_boards/tests100evil81.txt" NUM_TESTS="100"
 #make FILE_NAME="sample_tests/9x9_boards/tests1000evil81.txt" NUM_TESTS="1000"
 #make FILE_NAME="sample_tests/9x9_boards/tests10000easy81.txt" NUM_TESTS="10000"
 #make FILE_NAME="sample_tests/9x9_boards/tests10000evil81.txt" NUM_TESTS="10000"
-
 #16x16 boards
 #make FILE_NAME="sample_tests/16x16_boards/tests100easy256.txt" NUM_TESTS="100"
 #make FILE_NAME="sample_tests/16x16_boards/tests100evil256.txt" NUM_TESTS="100"
